07d81ee7ad7c2ef352bac6c0c3bf161a04813de0
Williane <williane.tads@gmail.com>
2021-08-28 21:46:18 -0300

    add aside with menu icons

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const { ipcRenderer } = require("electron");
 const btnClose = document.querySelector(".close");
 const btnMinimize = document.querySelector(".minimize");
 const btnMaximize = document.querySelector(".maximize");
+const menuBuIt = document.querySelector("#bu-it");
+const iframe = document.createElement("iframe");
 
 btnClose.onclick = () => {
   ipcRenderer.send("close-window-mac");
@@ -14,4 +16,9 @@ btnMinimize.onclick = () => {
 
 btnMaximize.onclick = () => {
   ipcRenderer.send("maximize-window-mac");
+};
+
+menuBuIt.onclick = () => {
+  menuBuIt.classList.add("menu-active");
+  iframe.classList.remove("not-activate");
 };
