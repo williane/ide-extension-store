88d05139c2d2e28c34d604fc7f9b6380af62d957
Williane <williane.tads@gmail.com>
2021-08-28 22:42:21 -0300

    add function remove class menu-active of menu icons

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const btnClose = document.querySelector(".close");
 const btnMinimize = document.querySelector(".minimize");
 const btnMaximize = document.querySelector(".maximize");
+const menuWrapper = document.querySelectorAll(".menu-icon-wrapper");
 const menuBuIt = document.querySelector("#bu-it");
 const menuGit = document.querySelector("#git-wrapper");
 const labelUsername = document.querySelector("#username");
@@ -11,6 +12,12 @@ const labelUsername = document.querySelector("#username");
 const userName = process.env["USERPROFILE"].split(path.sep)[2];
 
 labelUsername.textContent = userName;
+
+function removeClassActive() {
+  menuWrapper.forEach((item) => {
+    item.classList.remove("menu-active");
+  });
+}
 
 btnClose.onclick = () => {
   ipcRenderer.send("close-window-mac");
@@ -25,11 +32,13 @@ btnMaximize.onclick = () => {
 };
 
 menuBuIt.onclick = () => {
+  removeClassActive();
   menuBuIt.classList.add("menu-active");
   ipcRenderer.send("bu-it-open");
 };
 
 menuGit.onclick = () => {
+  removeClassActive();
   menuGit.classList.add("menu-active");
   ipcRenderer.send("bu-it-close");
 };
